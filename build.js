7e46458508e59426c26a749ddd73a9ca206818b3
Salisbury <salisbury.espinosa@gmail.com>
2016-04-24 00:59:39 +0300

    init - electron for react

---
+++ build.js
@@ -0,0 +1,8 @@
+var path = require('path');
+var webpack = require('webpack');
+var config = require('./webpack.config');
+
+var compiler = webpack(config);
+compiler.run(function (err, stats) {
+    console.log(err); // по завершению, выводим всю статистику
+});
